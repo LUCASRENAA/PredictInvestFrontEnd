ef289c85e6b1290ce6e22eef9da1b20f13cf1f4b
Nathalia Barbosa <nathaliaferdab@gmail.com>
2024-04-07 00:04:04 -0300

    Editado página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
-        <p className="text-lg mt-4">Get started by editing</p>
+        <h1 className="text-4xl font-bold">Bem-vindo ao Predict Invest</h1>
+        <p className="text-lg mt-4">Acompanhe seus investimentos</p>
       </div>
     </main>
   );
