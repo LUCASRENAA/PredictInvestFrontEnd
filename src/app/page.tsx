dccb021f8123a3ba5f3d0b7939596b6604d68259
Nathalia Barbosa <nathaliaferdab@gmail.com>
2024-04-07 13:58:17 -0300

    Mudado texto da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Bem-vindo ao Predict Invest</h1>
-        <p className="text-lg mt-4">Acompanhe seus investimentos</p>
+        <p className="text-lg mt-4 w-1/2">
+          O principal objetivo do programa é facilitar o acompanhamento e a análise dos indicadores financeiros e operacionais da empresa, proporcionando uma visão clara e organizada de sua performance.
+        </p>
       </div>
     </main>
   );
